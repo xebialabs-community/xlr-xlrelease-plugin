69cb070ee501dcab225b67aee75064f757476b68
jdewinne <joris.dewinne@gmail.com>
2015-03-04 16:04:33 +0100

    Search for pending tasks

--- src/main/resources/xlr/GetTaskId.py
+++ src/main/resources/xlr/GetTaskId.py
@@ -12,17 +12,12 @@ tasks = taskApi.searchTasksByTitle(taskTitle, None, currentRelease.id)
 taskPath = None
 for task in tasks:
     print "Received task with title: [%s].\n" % task.title
-    if task.title == taskTitle:
-        taskPath = task.id
+    if task.title == taskTitle and task.status == "PLANNED":
+        taskId = task.id
         break
 
-if taskPath:
-    print "Task with title [%s] found with id [%s].\n" % (taskTitle, taskPath)
-    taskPath = taskPath.replace('/','-')
-    prefix = 'Applications-'
-    taskId = taskPath
-    if taskPath.startswith(prefix):
-       taskId = taskPath[len(prefix):]
+if taskId:
+    print "Task with title [%s] found with id [%s].\n" % (taskTitle, taskId)
     sys.exit(0)
     
 print "Task with title [%s] not found.\n" % taskTitle
