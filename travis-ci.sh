93c160846d411bc3ae95d3e7b42a612cfaf1fd75
Joris De Winne <joris.dewinne@gmail.com>
2018-07-05 13:48:59 -0700

    Fixing travis build

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p ./xl-licenses
 http --check-status --print=b --auth $username:$password --json POST https://download.xebialabs.com/api/temporary/xl-release firstName=xlc lastName=xlc email=xlc@xebialabs.com company=xebialabs | jq --raw-output '.license' > ./xl-licenses/xl-release-license.lic
 ./gradlew clean assemble
-echo "plugin_jar=`ls build/libs | sort -n | head -1`" > src/test/resources/docker/.env
+export plugin_jar=`ls build/libs | sort -n | head -1`
 docker-compose -f src/test/resources/docker/docker-compose.yml up -d
 docker wait credentials_updater
 docker logs xlr
