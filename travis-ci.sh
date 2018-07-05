79ca9fd015e0e629f8f41ee33634a62ffef196c0
Joris De Winne <joris.dewinne@gmail.com>
2018-07-05 14:27:56 -0700

    Fixing codacy errors

--- travis-ci.sh
+++ travis-ci.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p ./xl-licenses
 http --check-status --print=b --auth $username:$password --json POST https://download.xebialabs.com/api/temporary/xl-release firstName=xlc lastName=xlc email=xlc@xebialabs.com company=xebialabs | jq --raw-output '.license' > ./xl-licenses/xl-release-license.lic
 ./gradlew clean assemble
-export plugin_jar=`ls build/libs | sort -n | head -1`
+export plugin_jar=$(ls build/libs | sort -n | head -1)
 docker-compose -f src/test/resources/docker/docker-compose.yml up -d
 docker wait credentials_updater
 docker logs xlr
