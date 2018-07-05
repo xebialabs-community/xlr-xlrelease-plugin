e5b2df91a693071eca7cf90bff36f43b071235fb
Joris De Winne <joris.dewinne@gmail.com>
2018-07-05 14:09:27 -0700

    Fixing travis build

--- travis-ci.sh
+++ travis-ci.sh
@@ -9,5 +9,5 @@ export plugin_jar=`ls build/libs | sort -n | head -1`
 docker-compose -f src/test/resources/docker/docker-compose.yml up -d
 docker wait credentials_updater
 docker logs xlr
-./gradlew itest
+./gradlew itest -PCHROME_HEADLESS_MODE=true
 docker-compose -f src/test/resources/docker/docker-compose.yml stop
